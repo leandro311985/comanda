1457b44dcacc888331203ad146c063feb3977879
leandro <leandro.alves311985@gmail.com>
2020-08-08 13:59:59 -0300

    commit tela de criar usuario

--- app/src/main/java/com/example/mylogin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/mylogin/ui/login/LoginActivity.kt
@@ -9,8 +9,10 @@ import com.example.mylogin.AuthListener
 import com.example.mylogin.ErroFragment
 import com.example.mylogin.R
 import com.example.mylogin.databinding.ActivityLoginBinding
+import com.github.loadingview.LoadingDialog
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.main_activity.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.kodein
 import org.kodein.di.generic.instance
@@ -22,6 +24,8 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
     override val kodein by kodein()
     private val factory: AuthViewModelFactory by instance()
     lateinit var auth:FirebaseAuth
+    private lateinit var dialog: LoadingDialog
+
 
 
     private lateinit var viewModel: AuthViewModel
@@ -40,11 +44,14 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
     }
 
     override fun onStarted() {
-        progressbar.visibility = View.VISIBLE
+       // progressbar.visibility = View.VISIBLE
+        dialog = LoadingDialog.get(this).show()
     }
 
     override fun onSuccess() {
-        progressbar.visibility = View.GONE
+//        progressbar.visibility = View.GONE
+        dialog.hide()
+
         if (auth.currentUser?.uid == MANAGER){
             startManagerActivity()
         }else
@@ -52,7 +59,8 @@ class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
     }
 
     override fun onFailure(message: String) {
-        progressbar.visibility = View.GONE
+//        progressbar.visibility = View.GONE
+        dialog.hide()
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, ErroFragment.newInstance(message))
             .addToBackStack(null)
